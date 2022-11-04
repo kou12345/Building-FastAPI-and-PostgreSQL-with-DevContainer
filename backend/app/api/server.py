ddcb24f03cf03c2e11dbbbba90e8f0996612b7f5
kou12345 <hi.0412.ko.2002@gmail.com>
2022-11-04 10:56:14 +0000

    alembicを使用してマイグレートを実行

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -1,10 +1,11 @@
 from app.api.routes import router as api_router
+from app.core import config, tasks
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
 def get_application():
-    app = FastAPI(title="Hedgehog Reservation", version="1.0.0")  # type: ignore
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
 
     app.add_middleware(
         CORSMiddleware,
@@ -13,6 +14,9 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.close_db_connection(app))  # type: ignore
 
     app.include_router(api_router, prefix="/api")
 
